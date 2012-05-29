e764e5c7b0321e8a0afac6a34838d26f43e670f0
Konstantin Kosmatov <key@kosmatov.su>
2012-05-29 13:05:10 +0400

    Fix application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
 
-  helper_method :current_user, :current_user?, :signed_in?, :sign_in :sign_out
+  helper_method :current_user, :current_user?, :signed_in?, :sign_in, :sign_out
 end
